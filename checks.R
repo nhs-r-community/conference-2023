9f326ab1bbb88a042200af24cf921540dd7502fe
Chris Beeley <chris.beeley@gmail.com>
2023-07-25 16:26:51 +0100

    Clean filepaths and add purrr::map_df

--- checks.R
+++ checks.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(purrr)
 library(janitor)
 
-abstracts <- read_excel("NHS-R Community Conference 2023 Call for Abstracts - Final List & Review Tracker V1.2.xlsx", 
+abstracts <- read_excel("secrets\\NHS-R Community Conference 2023 Call for Abstracts - Final List & Review Tracker V1.2.xlsx", 
                         skip = 1) |> 
   janitor::clean_names() |> 
   select(user_no,
@@ -13,11 +13,11 @@ abstracts <- read_excel("NHS-R Community Conference 2023 Call for Abstracts - Fi
 # list of reviewers vector
 source(paste0(here::here(), "/secrets/", "source_file.R"))
 
-purrr::map(
+df <- purrr::map_df(
   .x = list_reviewers,
   .f = function(name) {
     
-    file_name <- glue::glue("C:\\Users\\zoe.turner\\Midlands and Lancashire CSU\\NHS-R Team - Conference\\Conference 2023\\Abstracts\\Peer Review\\{file_name}\\NHS-R Abstract Review Scorecard - {file_name}.xlsx",
+    file_name <- glue::glue("secrets\\{file_name}\\NHS-R Abstract Review Scorecard - {file_name}.xlsx",
                             file_name = name)
     
     person_data <- read_excel(file_name,
@@ -27,9 +27,8 @@ purrr::map(
     abstracts |> 
       filter(reviewer_1 == name | reviewer_2 == name) |> 
       left_join(person_data, join_by(user_no == x1)) |> 
-      filter(is.na(x8))
+      filter(!is.na(x8))
   }
 )
 
-# name_file <- stringr::word(file_name, -2, -1)
-# name <- stringr::str_remove(name_file, ".xlsx")
+writexl::write_xlsx(df, "final.xlsx")
